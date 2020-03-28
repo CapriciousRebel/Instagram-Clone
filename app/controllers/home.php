dbd6f80fea8247c7356670c1bc7ea73ea6700207
Smith <codingfreak98@Smiths-MacBook-Pro.local>
2020-03-28 16:14:48 +0530

    nav bar has userimage

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -10,11 +10,15 @@ class Home
 
         if ($_SESSION["logged-in"] == 1) {
 
+            $user_id = $_SESSION['user_id'];
+            $user = \Model\User::getUser_id($user_id);
+
             $posts = \Model\Post::getPosts();
             shuffle($posts);
 
             echo \View\Loader::make()->render("templates/homepage.twig", array(
-                "posts" => $posts
+                "posts" => $posts,
+                "user" => $user
             ));
         } else {
             echo \View\Loader::make()->render("templates/home.twig");
